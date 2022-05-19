aa6b5d142ff87e7d03155599faaec5e6cb298aa8
Gabriel Paez <gpaez1992@gmail.com>
2022-05-19 16:21:31 -0300

    modificación en navbar

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -7,7 +7,7 @@ const CartWidget = () => {
   const {calcularCantProductos, subTotal} = useContext(CartContext)
 
   return (
-  <div className="dropdown dropdown-end order-3 md:ml-10 sm:ml-8">
+  <div className="dropdown dropdown-end order-3 md:ml-44 sm:ml-8">
     <label tabIndex="0" className="btn btn-ghost btn-circle md:mr-2">
       <div className="indicator text-paleta-colorFondo">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" ><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,5 @@
 import Item from "./Item"
-import {useEffect, useRef, useState} from "react"
+import {useEffect, useState} from "react"
 import { useParams } from "react-router-dom"
 import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
 
