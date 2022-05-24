99ebc081ba1a3de92d21c5eaf069f36a0ff5ad83
Gabriel Paez <gpaez1992@gmail.com>
2022-05-23 23:41:28 -0300

    modificaciones en index.js - variables de entorno

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_APIKEY,
-  authDomain: REACT_APP_AUTHDOMAIN,
-  projectId: REACT_APP_PROJECTID,
-  storageBucket: REACT_APP_STORAGEBUCKET,
-  messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-  appId: REACT_APP_APPID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 const app = initializeApp(firebaseConfig);
